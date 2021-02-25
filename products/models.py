c91175e8cee5eda88809efc14dc65b0047e2e986
Josh Smith <smitty1138@gmail.com>
2021-02-25 09:15:48 -0500

    Initial django framework commit.

---
+++ products/models.py
@@ -0,0 +1,20 @@
+from django.db import models
+
+class Category(models.Model):
+    name = models.CharField(max_length=255)
+    description = models.CharField(max_length=255)
+    published_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+class Candle(models.Model):
+    title = models.CharField(max_length=255)
+    published_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+    subtitle = models.CharField(max_length=255)
+    description = models.CharField(max_length=500)
+    price = models.DecimalField(max_digits=6, decimal_places=2)
+    categories = models.ManyToManyField(Category, related_name='candles')
+
+class ProductImage(models.Model):
+    candle = models.ForeignKey(Candle, on_delete=models.CASCADE)
+    image = models.ImageField(upload_to='products/images/')
